911c6d782ff126a562cb83a898101b84b4a7a542
Thomas Solyan <tsolyan@yahoo.com>
2014-08-18 12:07:53 -0500

    Update app.js

--- nodeSite/app.js
+++ nodeSite/app.js
@@ -5,22 +5,24 @@ var express = require('express'),
     logger = require('morgan'),
     cookieParser = require('cookie-parser'),
     bodyParser = require('body-parser'),
+    // TODO Lock Down DB via node passport...
     mongo = require('mongodb'),
     monk = require('monk'),
-    db = monk('localhost:27017/ec-nodesite'),
+    db = monk('<login info here>'),// not for GIT hub
     routes = require('./routes/index'),
     users = require('./routes/users'),
     app = express();
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
-
+app.set('port', process.env.PORT || 8080);
 app.use(favicon());
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded());
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.listen(app.get('port'));
 app.use(function(req,res,next){
     req.db = db;
     next();
