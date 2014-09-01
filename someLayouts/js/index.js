b0cccb576745667abf75a30dbabcebddcce97433
tsolyan <tsolyan@yahoo.com>
2014-09-01 09:32:14 -0700

    new method setTimes

--- someLayouts/js/index.js
+++ someLayouts/js/index.js
@@ -8,7 +8,7 @@ var JZ = (function() {
         var parent = $(ev.target).parents('li'),
             newImage = parent.find('img.news-thumb').attr('src'),
             newHeader = parent.find('.tile-head').text(),
-            newCopy = parent.find('.tile-article').text(),
+            newCopy = parent.find('.tile-article').first().text(),
             readMore = parent.data('url');
         $('[data-image]').attr('src', newImage);
         $('[data-head]').text(newHeader);
@@ -24,6 +24,7 @@ var JZ = (function() {
           (isTruncated) ? $('[data-more]').show() : $('[data-more]').hide();
         }
       });
+      $('[data-trim]').dotdotdot();
     },
 
     setIE : function() {
@@ -36,10 +37,42 @@ var JZ = (function() {
       }
     },
 
+    setTimers: function() {
+      var today = new Date(),
+          hour = today.getUTCHours() + 7,
+          min = (today.getMinutes() < 10) ? '0' + today.getMinutes() : today.getMinutes(),
+          displayDate = (today.getFullYear()) + '-' + (today.getMonth() + 1) + '-' + (today.getDate()),
+          timeValue = "" + ((hour > 12) ? hour -12 : hour),
+          meridian = (timeValue >= 12) ? "PM" : "AM",
+          pacific = "" + (((hour - 3) > 12) ? (hour - 3) -12 : (hour - 3)),
+          pacMeridian = (pacific >= 12) ? "PM" : "AM",
+          dublin = "" + (((today.getUTCHours() + 1) > 12) ? (today.getUTCHours() + 1) -12 : (today.getUTCHours() + 1)),
+          dubMeridian = ((timeValue + 7) >= 12) ? "PM" : "AM",
+          munich = "" + (((today.getUTCHours() + 2) > 12) ? (today.getUTCHours() + 2) -12 : (today.getUTCHours() + 2)),
+          munMeridian = ((timeValue + 8) >= 12) ? "PM" : "AM";
+      $('.times li:eq(0) p').html('<p class="hr-min">' + pacific + ":" + min + '<span class="after">' +  pacMeridian + '</span><br><span class="next">'+ displayDate + '</p>');
+      $('.times li:eq(1) p').html('<p class="hr-min">' + timeValue + ":" + min + '<span class="after">' +  meridian + '</span><br><span class="next">'+ displayDate + '</p>');
+      $('.times li:eq(2) p').html('<p class="hr-min">' + dublin + ":" + min + '<span class="after">' +  dubMeridian + '</span><br><span class="next">'+ displayDate + '</p>');
+      $('.times li:eq(3) p').html('<p class="hr-min">' + munich + ":" + min + '<span class="after">' +  munMeridian + '</span><br><span class="next">'+ displayDate + '</p>');
+
+      $('.date').each(function(i,el) {
+        var newDate = $(el).text();
+        newDate = new Date(newDate);
+        newDate = newDate.toString().split(' ').splice(1, 2).join(' ');
+        $(el).html(newDate)
+      });
+    },
+
     init : function() {
-      JZ.replaceContent();
-      JZ.addEllipsis();
+      if ($('.news-wrapper').length) {
+        JZ.replaceContent();
+        JZ.addEllipsis();
+      }
       JZ.setIE();
+      if ($('.times').length) {
+        JZ.setTimers();
+        setInterval(JZ.setTimers, 10000);
+      }
     }
   };
 })();
