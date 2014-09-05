346bc83bb90d9d8a0011a35f6c53df879737f659
unknown <tom@SP2013DEV-REYES.sp2013dev.com>
2014-09-05 09:34:33 -0500

    copy of sitedata as jsObj

--- TPSite/public/data/siteData.js
+++ TPSite/public/data/siteData.js
@@ -1,50 +1,133 @@
 var siteData = [{
-  "title" : "Career Development for Millennials: the NowEnterprise for the Next Generation", 
-  "pubDate" : "Wed, 02 Apr 2014 21:02:40 +0000", 
-  "creator" : "Mike Davidson", 
-  "imageLocation" : "images/blog/blog-1.jpg", 
-  "displayTitle" : "get ready for",
-  "displayTitleL" : "the millennials"
-},
-{
-  "title" : "The Future of the Knowledge Worker is the NowEnterprise",
-  "displayTitle" : "future of the",
-  "displayTitleL" : "knowledge worker",
-  "pubDate" : "Mon, 17 Mar 2014 21:15:43 +0000",
-  "creator" : "David Sidwell",
-  "imageLocation" : "images/blog/blog-2.jpg"
-},
-{
-  "title" : "Goodbye Intranet. Say Hello to the NowEnterprise",
-  "displayTitle" : "goodbye intranet,",
-  "displayTitleL" : "hello to the nowentreprise",
-  "pubDate" : "Mon, 13 Jan 2014 21:17:36 +0000",
-  "creator" : "Ralph Keipert",
-  "imageLocation" : "images/blog/blog-3.jpg"
-},
-{
-  "title" : "Enterprise Collaboration Obstacles: Breaking Down Organizational Silos",
-  "displayTitle" : "collaboration",
-  "displayTitleL" : "obstacles",
-  "pubDate" : "Wed, 26 Jun 2013 20:08:19 +0000",
-  "creator" : "Ralph Keipert",
-  "imageLocation" : "images/blog/blog-4.jpg"
-},
-{
-  "title" : "Enterprise Collaboration Strategy and Roadmap",
-  "displayTitle" : "Enterprise Collaboration",
-  "displayTitleL" : "Strategy & Roadmap",
-  "pubDate" : "Mon, 17 Jun 2013 19:50:00 +0000",
-  "creator" : "David Sidwell",
-  "imageLocation" : "images/blog/blog-5.jpg"
-},
-{
-  "title" : "Enterprise Collaboration: User Experience and Change Resilient Design",
-  "displayTitle" : "Collaboration: Usability",
-  "displayTitleL" : "& Change Resilient Design",
-  "pubDate" : "Wed, 09 Oct 2013 15:01:24 +0000",
-  "creator" : "Mike Davidson",
-  "imageLocation" : "images/blog/blog-6.jpg"
+  "engagement":[
+    {
+      "title" : "SERVICE OFFERINGS",
+      "copyInvis" : "", 
+      "copy" : "Standards for Web Design are higher than ever. Learn how we're creating websites", 
+      "imageLocation" : "images/blog/engagement1.jpg",
+      "linkInvis" : "remove",
+      "links" : [
+        {"name": "", "url": ""},
+        {"name": "", "url": ""},
+        {"name": "", "url": ""}
+      ],
+      "button" : ""
+    },
+    {
+      "title" : "FEATURED PROJECTS",
+      "copyInvis" : "remove", 
+      "copy" : "", 
+      "imageLocation" : "images/blog/engagement2.jpg",
+      "linkInvis" : "",
+      "links" : [
+        {"name": "Health Care Service Corporation", "url": ""},
+        {"name": "Blue Cross Blus Sheild of IL", "url": ""},
+        {"name": "Dearborn National", "url": ""}
+      ],
+      "button" : "remove"
+    },
+    {
+      "title" : "TOP PICKS",
+      "copyInvis" : "remove", 
+      "copy" : "", 
+      "imageLocation" : "images/blog/engagement3.jpg",
+      "linkInvis" : "",
+      "links" : [
+        {"name": "SMART Webinar: The evolution of Consumer-Facing Websites", "url": ""},
+        {"name": "Delightful Customer Experiences: IT and Marketing Must Play Nice", "url": ""},
+        {"name": "Customer Experience and Digital Innovation", "url": ""}
+      ],
+      "button" : "remove"
+    }
+  ],
+  "productivity":[
+    {
+      "title" : "SERVICE OFFERINGS",
+      "copyInvis" : "", 
+      "copy" : "Collaborative tools that make work smarter, better, faster (and more mobile) get adopted. Find out how.", 
+      "imageLocation" : "images/blog/productivity1.jpg",
+      "linkInvis" : "remove",
+      "links" : [
+        {"name": "", "url": ""},
+        {"name": "", "url": ""},
+        {"name": "", "url": ""},
+        {"name": "", "url": ""}
+      ],
+      "button" : ""
+    },
+    {
+      "title" : "FEATURED PROJECTS",
+      "copyInvis" : "remove", 
+      "copy" : "", 
+      "imageLocation" : "images/blog/productivity2.jpg",
+      "linkInvis" : "",
+      "links" : [
+        {"name": "TreeHouse Foods", "url": ""},
+        {"name": "Huron Consulting Group", "url": ""},
+        {"name": "Navistar", "url": ""},
+        {"name": "U.S. Army Corps of Engineers", "url": ""}
+      ],
+      "button" : "remove"
+    },
+    {
+      "title" : "TOP PICKS",
+      "copyInvis" : "remove", 
+      "copy" : "", 
+      "imageLocation" : "images/blog/productivity3.jpg",
+      "linkInvis" : "",
+      "links" : [
+        {"name": "SharePoint 2013 Assesment & Report", "url": ""},
+        {"name": "Say Hello to the Now Enterprise", "url": ""},
+        {"name": "Enterprise Collaboration Roadmap", "url": ""},
+        {"name": "Why an App isn't an Application", "url": ""}
+      ],
+      "button" : "remove"
+    }
+  ],
+  "insurance":[
+    {
+      "title" : "SERVICE OFFERINGS",
+      "copyInvis" : "", 
+      "copy" : "Smart web and mobile experiences engage, stimulate, and drive buyer and member behaviour.", 
+      "imageLocation" : "images/blog/insurance1.jpg",
+      "linkInvis" : "remove",
+      "links" : [
+        {"name": "", "url": ""},
+        {"name": "", "url": ""},
+        {"name": "", "url": ""},
+        {"name": "", "url": ""}
+      ],
+      "button" : ""
+    },
+    {
+      "title" : "FEATURED PROJECTS",
+      "copyInvis" : "remove", 
+      "copy" : "", 
+      "imageLocation" : "images/blog/insurance2.jpg",
+      "linkInvis" : "",
+      "links" : [
+        {"name": "Health Care Service Corporation", "url": ""},
+        {"name": "Blue Cross Blus Sheild of IL", "url": ""},
+        {"name": "Medical Mutual of Ohio", "url": ""},
+        {"name": "Dearborn National", "url": ""}
+      ],
+      "button" : "remove"
+    },
+    {
+      "title" : "TOP PICKS",
+      "copyInvis" : "remove", 
+      "copy" : "", 
+      "imageLocation" : "images/blog/insurance3.jpg",
+      "linkInvis" : "",
+      "links" : [
+        {"name": "Build Loyalty, Decrease Costs: Put your Member Portal to Work", "url": ""},
+        {"name": "Uncover Opportunities to Connect", "url": ""},
+        {"name": "What Your Portal UI Says About You", "url": ""},
+        {"name": "The Health Care Transparency Toolbox", "url": ""}
+      ],
+      "button" : "remove"
+    }
+  ]
 },
 {
   "name" : "Hollister Healthcare",
@@ -53,12 +136,12 @@ var siteData = [{
 },
 {
   "name" : "Huron Consulting",
-  "copy" : "What does an award-winning intranet look like? Like increasing usage by 86% and tripling the number of content contributors.",
+  "copy" : "An award-winning intranet puts the right information in the hands of the right people at the right time.",
   "imageUrl" : "images/portfolio/port-2.jpg"
 },
 {
   "name" : "Navistar",
-  "copy" : "Finding thousands of untapped employee ideas - and uncovering 5.3M in savings. That's pretty heroic.",
+  "copy" : "A social business solution connects employees and captures ideas - delivering $5.3M in savings in the first sixty days.",
   "imageUrl" : "images/portfolio/port-3.jpg"
 },
 {
@@ -67,88 +150,122 @@ var siteData = [{
   "imageUrl" : "images/portfolio/port-4.jpg"
 },
 {
-  "mainLinks" : [ "home", "challenge", "solution", "portfolio", "get-started" ],
+  "name" : "US Army Corps",
+  "copy" : "A global enterprise collaboration platform - one of the largest SharePoint installations within the Department of Defense - becomes a model for other agencies.",
+  "imageUrl" : "images/portfolio/port-4.jpg"
+},
+{
+  "name" : "Blue Cross Blue Sheild",
+  "copy" : "On the heels of Health Care Reform, an innovative e-retailing solution sets a new standard in consumer engagement.",
+  "imageUrl" : "images/portfolio/port-4.jpg"
+},
+{
+  "mainLinks" : [ "home", "expertise", "results", "about-us", "blog", "contact" ],
   "el" : "#"
 },
 {
-  "mainCopy" : "SHAREPOINT, A HERO'S TALE",
-  "secondaryCopy" : "Are you ready to go from",
-  "tertiaryCopy" :  "scapegoat to hero?"
-},
-{
-  "linkName" : [ "problem", "implication", "trends", "answer", "benefits", "how", "what" ],
+  "linkName" : [
+    {"link": "customer engagement", "id_":"engagement"},
+    {"link":"workforce productivity", "id_":"productivity"},
+    {"link": "health insurance", "id_":"insurance"},
+    "about",
+    "leadership",
+    "partners",
+    "careers"
+  ],
   "el" : "#"
 },
 {
   "images" : [
-    { "id_" : "problem_1","imageUrl" : "images/charts/chart" },
-    { "id_" : "implication_1", "imageUrl" : "images/charts/placeholder.jpg" },
-    { "id_" : "trends_1", "imageUrl" : "images/charts/placeholder.jpg" },
-    { "id_" : "answer_1", "copy": '"... it became apparent that we needed to construct a strategy before we started talking about long-term funding much less executing anything."' },
-    { "id_" : "benefits_1", "imageUrl" : "images/charts/chart1.png" },
-    { "id_" : "how_1", "imageUrl" : "images/charts/chart2.png" },
-    { "id_" : "what_1", "imageUrl" : "images/charts/chart3.png" }
+    { "id_" : "engagement_1","imageUrl" : "images/charts/engagement" },
+    { "id_" : "productivity_1", "imageUrl" : "images/charts/productivity" },
+    { "id_" : "insurance_1", "imageUrl" : "images/charts/insurance" },
+    { "id_" : "about_1", "imageUrl": "images/charts/aboutus", "invis": "" },
+    { "id_" : "leadership_1", "imageUrl" : "", "invis": "remove" },
+    { "id_" : "partners_1", "imageUrl" : "images/charts/partners", "invis": "" },
+    { "id_" : "careers_1", "imageUrl" : "images/charts/careers", "invis": "" }
   ]
 },
 {
   "data" : [
     {
-      "id_" : "problem_2",
-      "h2" : "problem",
-      "h4" : "There's no doubt about it - you're not alone;",
-      "h4br" : "implementing SharePoint can be challenging.",
-      "para1" : "A 2013 Industry Watch Report from AIIM found that 61% of SharePoint ECM projects had stalled or did not meet expectations.",
-      "heavy" : "And, as an IT leader, you've heard the complaints..."
+      "id_" : "engagement_2",
+      "h2" : "customer engagement",
+      "h4" : "We engage with brands that we enjoy, and that means your website.",
+      "para1" : "Customer engagement is not just a buzzword - in a digital world, customer engagement is critical. We help forward-thinking organizations develop multi-channel digital solutions that captivate consumers and drive results. Engaging your customers where they live? That's smart.",
+      "heavy" : "Watch the new rules for web design",
+      "invis": "remove",
+      "trigger" : "engagement"
     },
     {
-      "id_" : "implication_2",
-      "h2" : "implication",
-      "h4" : "Even after you get SharePoint live, users still aren't happy.",
-      "para1" : '"SharePoint isn’t easy to use."',
-      "para1br" : '"I can’t find what I’m looking for."',
-      "para1br1" : '"It dosen’t make me more productive. It actually makes my job harder."',
-      "para1br2" : '"I’d rather walk on hot coals... (well maybe not yet)"',
-      "para2": 'And then your team weighs in, "This is a mess. I can’t support it."',
-      "heavy" : "And there is the future to consider..."
+      "id_" : "productivity_2",
+      "h2" : "workforce productivity",
+      "h4" : "The Now Enterprise is not just about technology. It's about people.",
+      "para1" : "Forget static intranets. The now enterprise is always moving. We deliver collaboration, social, mobile, and workflow in an integrated productivity solution. Corporate technology that matches the power and simplicity of consumer tools? That's smart.",
+      "heavy" : "See the new social enterprise",
+      "invis": "",
+      "trigger" : "productivity"
     },
     {
-      "id_" : "trends_2",
-      "h2" : "trends",
-      "h4" : "Future trends compound the challenges.",
-      "para1" : "Millennials will soon make up 75% of the workforce.",
-      "para1br" : "Tablets are outselling PC's and smart phones are ubiquitous.",
-      "para1br1" : "Social technology has changed the way people interact.",
-      "heavy" : "Becoming the hero seems as though it will only get harder.",
-      "heavy2" : "So, where can you go from here?"
+      "id_" : "insurance_2",
+      "h2" : "health insurance",
+      "h4" : "30 million people are joining the ranks of the newly insured... what is your digital strategy?",
+      "para1" : "As the Affordable Care Act shifts the healthcare landscape, leading insurers look to us to help redefine their digital stragety and transform the customer experience. Partnering with an industry expert to tackle the challenges of reform? That's smart.",
+      "heavy" : "Envision a Connected Experience",
+      "invis": "remove",
+      "trigger" : "insurance"
     },
     {
-      "id_" : "answer_2",
-      "h2" : "answer",
-      "h4" : "Can you go from SharePoint scapegoat to hero and save the day? ",
-      "h4br" : "Yes you can.",
-      "para1" : "The answer starts with an Enterprise Collaboration Roadmap.",
-      "para2" : "Galvanize your organization around a common set of priorities and set a course for high adoption. It's amazing what buy-in and alignment can do",
-      "heavyUrl": "http://blog.tahoepartners.com/index.php/enterprise-collaboration-strategy-ready-fire-aim/",
-      "heavy" : "READ HOW ONE CLIENT DID IT"
+      "id_" : "about_2",
+      "h2" : "about",
+      "h4" : "Digital solutions drive results",
+      "para1" : "Tahoe Partners links digital strategy, design, content, and technology to engage customers and maximize performance. Our solutions are designed around people. Our solutions are adopted. Our solutions live at the nexus of web, mobile, and social.",
+      "invis" : "",
+      "buttonCopy": "subscribe to our blog",
+      "buttonTarget" : "#blog_subscription",
+      "boss" : "remove",
+      "bosses" : []
+
     },
     {
-      "id_" : "benefits_2",
-      "h2" : "benefits",
-      "h4" : "Being a hero is always better than being the scapegoat.",
-      "para1" : "When you plan ahead and start SharePoint projects from an enterprise collaboration roadmap, adoption increases and your stakeholders are aligned.",
-      "heavy" : "And it doesn't take much to get started..."
+      "id_" : "leadership_2",
+      "h2" : "leadership",
+      "h4" : "Industry leaders in emerging technologies",
+      "para1" : "As you experience our solutions, you'll see a reflection of the people who made them.",
+      "invis" : "remove",
+      "buttonCopy": "",
+      "buttonTarget": "",
+      "boss" : "",
+      "bosses" : [
+        {"bossName": "Ray Conrad", "bossTitle": "CHEIF EXECUTIVE OFFICER", "bossImg": "images/bosses/conrad.jpg", "blurb" : "Ray is a co-founder of Tahoe Partners with over 25 years of consulting and corporate IT experience delivering a wide range of technology solutions to boost workforce productivity."},
+        {"bossName": "Ralph Keipert", "bossTitle": "PRESIDENT", "bossImg": "images/bosses/keipert.jpg", "blurb" : "A Tahoe Partners co-founder with over twenty years of experience growing and leading consulting firms."},
+        {"bossName": "Bob Carpenter", "bossTitle": "CHEIF FINANCIAL OFFICER", "bossImg": "images/bosses/carpenter.jpg", "blurb" : "Bob is a Certified Public Accountant with over thirty years of experience improving operational and financial performance to maximize efficiency and profitability."},
+        {"bossName": "Russell Maloney", "bossTitle": "MANAGING DIRECTOR", "bossImg": "images/bosses/maloney.jpg", "blurb" : "A co-founder of Tahoe Partners, Russell is a senior Sales and Recruiting executive with over twenty years of experience and a proven track record of satisfied clients and partners."},
+        {"bossName": "Amy Widicus", "bossTitle": "MANAGING DIRECTOR", "bossImg": "images/bosses/widicus.jpg", "blurb" :"Amy brings twenty years of experience in delivering high-impact, high-value digital solutions."},
+        {"bossName": "Mike Crisanti", "bossTitle": "MANAGING DIRECTOR", "bossImg": "images/bosses/crisanti.jpg", "blurb" : "A pioneer in the commercialization of the web, Mike has more than twenty years of experience shepherding clients from internet infancy to Web 2.0."}
+      ]
     },
     {
-      "id_" : "how_2",
-      "h2" : "how it works",
-      "h4" : "Start with where you are.",
-      "para1" : "The process of developing a roadmap means taking stock of your current state – your sites, tools, infrastructure, and processes, leveraging best practices and our experience to help define your vision for the future, and prioritizing the initiatives required to move forward."
+      "id_" : "partners_2",
+      "h2" : "partners",
+      "h4" : "Best-in-class partners help us transform smart ideas into real results.",
+      "para1" : "Our industry-leading partners help us give our clients a competitive advantage. As a Gold Certified Partner, we bring expertise in Microsoft's Portals and Collaboration competency to everything we do.",
+      "invis" : "remove",
+      "buttonCopy": "",
+      "buttonTarget" : "",
+      "boss" : "remove",
+      "bosses" : []
     },
     {
-      "id_" : "what_2",
-      "h2" : "what it covers",
-      "h4" : "Turn Roadblocks into Building Blocks",
-      "para1" : "Your roadmap creates a clear path to the future vision for your SharePoint strategy. With solutions defined for each component, along with steps to achieve each goal, you will have the background to guide your organization in taking the right steps at the right time in the journey. And to become a SharePoint hero!"
+      "id_" : "careers_2",
+      "h2" : "careers",
+      "h4" : "We are always looking for talented people to join our team.",
+      "para1" : "Working smart is part of our culture. If you're bright, inventive, and entrepreneurial, you'll find a challenging environment that's fun and flexible.",
+      "invis" : "",
+      "buttonCopy": "search jobs",
+      "buttonTarget": "jobs",
+      "boss" : "remove",
+      "bosses" : []
     }
   ]
 }
