0c32b1b568b29b0d3cae44388c0ce41c56805469
unknown <tom@SP2013DEV-REYES.sp2013dev.com>
2014-09-05 08:19:31 -0500

    tpSlider updates

--- menus/js/tpSlider.js
+++ menus/js/tpSlider.js
@@ -1,8 +1,7 @@
 var tpSlider = (function() {
   'use strict';
-  
   var _slideWidth,
-      _elCount = 0,
+      _elCount,
       _width,
       $slider = $('[data-slide]'),
       $count = $('[data-count]');
@@ -40,23 +39,52 @@ var tpSlider = (function() {
             minus = '';
             baseMarg = 0;
             count = 0;
+            $('[data-tp] .prev-arrow').addClass('disabled');
           } else {
             minus = '-';
             baseMarg = _width * (count - 1);
             count--;
+            $('[data-tp] .prev-arrow').removeClass('disabled');
           }
           $slider.animate({
             marginLeft: minus + baseMarg + 'px'
           });
         }
+        tpSlider.setCount(count);
       });
     },
 
+    setCount : function(count) {
+      if ($slider.hasClass('news-slider')) {
+        var els;
+        $slider.find('li').each(function(i, el) {
+          i++;
+          els = i;
+        });
+        $('[data-total]').text(els);
+        var factor = 14,
+            rangeFloor = factor * count + 1,
+            rangeCeiling = factor * (count + 1);
 
-    init : function(el, elW) {
-      _width = elW;
+        if (els < factor) {
+          $count.text('1 - ' + els);
+        } else if ((els > factor) && (count === 0 || count === undefined)) {
+          $count.text('1 - ' + factor);
+        } else {
+          if (rangeCeiling > els) {
+            $count.text(rangeFloor + ' - ' + els);
+          } else {
+           $count.text(rangeFloor + ' - ' + rangeCeiling);
+          }
+        }
+      }
+    },
+
+    init : function(num, el, elW) {
+      _width = num;
       tpSlider.setWidth(el, elW);
       tpSlider.slideListener();
+      tpSlider.setCount();
     }
   };
 })();
